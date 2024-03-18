37598e7be7c5dc01a41a3a1dd403df24f3c34911
harshsoni003 <sonih3654@gmail.com>
2024-03-18 23:53:35 +0530

    add js part3

---
+++ JavaScript/script2.js
@@ -0,0 +1,86 @@
+//print
+for(let i=1; i<=20; i++){
+    console.log("HARSH SONI");
+}
+console.log("loop has ended");
+
+
+
+//calculate sum 1 to 5
+let sum=0;
+for (let i=1; i<=5; i++){
+    sum = sum+i;             //sum=0+1=1  (i=1)
+                            // sum=1+2=3 (i=2)
+
+}
+console.log("sum = ", sum);
+
+
+
+//print
+for(let i=1; i<=20; i++){
+    console.log("i", i);
+}
+console.log(i); //i=6; isn't print because i decleared in only block({})
+
+
+//while loop
+let j=1;
+while(j<4){
+    j++;
+    console.log("j", j);
+
+}
+
+
+//do-while loop
+let i=20;
+do {
+    
+    console.log("HARSH SONI");
+    i++;
+} while(i<=10);
+
+//for of loop
+let str = "HARSH SONI";
+let size=0;
+for (let i of str){
+   console.log("i=", i);
+   size++;
+}
+console.log("Size of string is: "+size);            //Size of string is: 10
+
+//or
+
+let heroes=["harsh","soni","pratik","kadam","tanmay" ]; 
+for (let hero of heroes){
+    console.log(hero);
+}
+
+
+
+
+//for in loop
+let student = {
+    name :"Harsh Soni",
+    age :35,
+    city :"Delhi",
+    ispass: true,
+};
+for (let key in student){
+    console.log("key = ", key, "\nvalues", student[key]);
+}
+
+
+
+//PRACTICES
+let gameNum=20;
+
+let userNum = prompt("Guess the no: ");
+while(userNum != gameNum){
+    userNum = prompt("you entered wrong no. try again");
+
+}
+console.log("Congratulation , guess Correct no ");
+
+
